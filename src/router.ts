0f28db32d5094dd524cab93d837ca203d1d11683
veronicaisabelpereira <veronicaisabelpereira80@gmail.com>
2023-11-24 15:33:07 -0300

    revision de path

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,8 @@ import { Router } from "@vaadin/router";
 
 const router = new Router(document.querySelector(".root"));
 router.setRoutes([
+  { path: "/welcome", component: "home-page" },
   { path: "/", component: "home-page" },
   { path: "/chatroom", component: "chat-page" },
-  //{ path: ".*", redirect: "/" },
+  { path: "(.*)", redirect: "/" },
 ]);
